c8623ffe7dd96307bb7dea407340f23ecefb3b76
Eric Pan <epan@users.noreply.github.com>
2017-04-26 10:47:09 -0700

    (feat) Serve static files in public folder

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const IP = '127.0.0.1';
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/client/public'));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
