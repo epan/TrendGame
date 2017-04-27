f16f99b58ceb4397c23600d2dbb56046d8ec64da
Eric Pan <epan@users.noreply.github.com>
2017-04-27 11:44:44 -0700

    (feat) Add backdate and test method for trend query

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ const morgan = require('morgan');
 
 const app = express();
 const IP = '127.0.0.1';
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/client/public'));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${IP}:${PORT}`);
-  trendQuery('turtles');
-});
+if (!module.parent) {
+  app.listen(PORT, () => {
+    console.log(`Listening on ${IP}:${PORT}`);
+    // trendQuery('turtles');
+  });
+}
 
 app.get('/api', (req, res) => {
   res.send({
