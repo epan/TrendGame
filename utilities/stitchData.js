4046ceafce4404c8d0a0ed0ee63a5148024105e2
Eric Pan <epan@users.noreply.github.com>
2017-05-06 14:10:26 -0700

    (refactor) Remove comments and add error handling

--- utilities/stitchData.js
+++ utilities/stitchData.js
@@ -1,31 +1,19 @@
-// For each story,
-  // Find timeline point with matching date
-  // Insert stories key from story into matching point
-// Return timeline
-
 module.exports = (timeline, stories) => {
-  const thisTimeline = timeline.slice();
   const timelineDates = timeline.map(point => point.date);
 
   stories.forEach(story => {
-    thisTimeline.forEach(point => {
+    // throw error if a story's date is not in the timeline
+    if (!timelineDates.includes(story.date)) {
+      throw new Error(`Story date matches no timeline dates: ${story}`);
+    }
+
+    // add stories to matching the timeline date
+    timeline.forEach(point => {
       if (point.date === story.date) {
         point['stories'] = story.stories;
       }
     });
   });
 
-  return thisTimeline;
-
-  // for (let story of stories) {
-  //   if (!timelineDates.includes(story.date)) {
-  //     throw new Error(`Story date matches no timeline dates: ${story}`);
-  //   }
-
-  //   timeline.forEach(point => {
-  //     if (point.date === story.date) {
-  //       point['stories'] = story.stories;
-  //     }
-  //   });
-  // }
+  return timeline;
 };
