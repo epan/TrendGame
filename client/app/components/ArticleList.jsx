f9836d73050129c5d2d2a12fbc91cd7c44f048f7
Eric Pan <epan@users.noreply.github.com>
2017-05-10 16:37:57 -0700

    (refactor) Use ternary for conditional on articles

--- client/app/components/ArticleList.jsx
+++ client/app/components/ArticleList.jsx
@@ -4,19 +4,17 @@ import Article from './Article.jsx';
 const ArticleList = ({ storyPoint }) => {
   let articles;
 
-  if (!storyPoint.hasOwnProperty('stories')) {
-    articles = <div>no story point yet</div>;
-  } else {
-    let { formattedTime, stories } = storyPoint;
-    articles = (
+  !storyPoint.hasOwnProperty('stories')
+  ? articles = <div>no story point yet</div>
+  : articles = (
       <div>
-        <h2>Top news stories for {formattedTime}</h2>
-        {stories.map(story => {
+        <h2>Top news stories for {storyPoint.formattedTime}</h2>
+        {storyPoint.stories.map(story => {
           return <Article key={story.url} story={story}/>;
         })}
       </div>
-    );
-  }
+  );
+
   return articles;
 };
 
